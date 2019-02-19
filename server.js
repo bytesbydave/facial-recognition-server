b890ab50d41408563303f50ad0e5f810a1caab0e
Dave <dave.lin21@gmail.com>
2019-02-19 01:05:58 -0500

    change host to postgres url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-opaque-69963',
     user: 'davelin',
     password: '',
     database: 'image-recognition'
